6bc654ebf69008f2caa7d0d836e86d0f24a5b94f
RonnyLincoln <mwithiamoki@gmail.com>
2020-11-21 17:28:33 +0300

    class user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth import get_user_model
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_ROLES = (
+        ('CA', 'COMPANY ADMIN'),
+        ('NU', 'NORMAL USER')
+    )
+    role = models.CharField(
+        verbose_name='user role', max_length=2, choices=USER_ROLES,default='NU'
+    )
+
