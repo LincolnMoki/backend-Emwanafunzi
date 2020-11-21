f15c4a93bee1e0ed2527db50f17b20fff07bd14c
RonnyLincoln <mwithiamoki@gmail.com>
2020-11-21 17:40:28 +0300

    created class user serializer

--- api/serializers.py
+++ api/serializers.py
@@ -26,3 +26,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'role']
