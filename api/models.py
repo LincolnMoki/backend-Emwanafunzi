d6294566915882e07a94cc7b7ddb345c955ddb64
RonnyLincoln <mwithiamoki@gmail.com>
2020-11-21 17:31:47 +0300

    class profile

--- api/models.py
+++ api/models.py
@@ -38,3 +38,18 @@ class Product(models.Model):
 
     def __str__(self):
         return '{} {}'.format(self.product_tag, self.name)
+        
+class Profile(models.Model):
+    user = models.OneToOneField( settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    profile_picture = CloudinaryField('image')
+    def __str__(self):
+            return f'{self.user.username} profile'
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance )
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()        
